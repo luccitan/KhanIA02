aad37c5989f5279a2bb604c6601ab9645b0845df
Tanguy LUCCI <lcc.tanguy@gmail.com>
2016-05-26 11:35:59 +0200

    Ajout du setPlayer et initPlayer en prédicats dynamiques

--- src/main.pl
+++ src/main.pl
@@ -1,3 +1,22 @@
 :- include('tools.pl').
 :- include('init.pl').
 :- include('engine.pl').
+
+/* 
+	initPlayer(PlayerColor, PlayerType, PlayerPieces)
+	------------------------------
+	Initialise le prédicat dynamique "joueur" de type "PlayerColor"
+	en affectant PlayerType et PlayerPieces dans une liste
+*/
+initPlayer(PlayerColor, PlayerType, PlayerPieces) :-
+	asserta((joueur(PlayerColor, PlayerType, PlayerPieces))).
+
+/* 
+	setPlayer(PlayerColor, PlayerType, PlayerPieces)
+	------------------------------
+	Modifie le prédicat existant dynamique "joueur" de type "PlayerColor"
+	en affectant PlayerType et PlayerPieces dans une liste
+*/
+setPlayer(PlayerColor, PlayerType, PlayerPieces) :-
+	retract(joueur(PlayerColor,_,_)),
+	asserta((joueur(PlayerColor, PlayerType, PlayerPieces))).
